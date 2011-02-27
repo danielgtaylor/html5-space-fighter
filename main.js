cdb8088cf703c1a556c94e42b7405e1f1bf8ad7b
Daniel G. Taylor <dan@programmer-art.org>
2011-02-26 19:11:52 -0500

    Significantly reduce CPU usage while paused, update game over screen to be a little more fancy

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function main() {
    
    var font = new gamejs.font.Font('48px ubuntu, sans-serif');
    var paused = font.render("Paused", "#08c");
-   var gameOver = font.render("Game Over", "#c00");
+   var gameOver = font.render("Game Over", "#000");
    
    var background = gamejs.image.load("images/nebula1.jpg");
    var backgroundPos = SIZE[1] - background.getSize()[1];
@@ -42,6 +42,8 @@ function main() {
    
    var hud = screen.Hud(SIZE);
    
+   var pausedDrawn = false;
+   
    // game loop
    var mainSurface = gamejs.display.getSurface();
    // msDuration = time since last tick() call
@@ -49,6 +51,19 @@ function main() {
          gamejs.event.get().forEach(function(event) {
             controls.handle(event);
          });
+         
+         if (controls.paused) {
+            if (!pausedDrawn) {
+               pausedDrawn = true;
+               // Draw paused overlay
+               gamejs.draw.rect(mainSurface, "rgba(0, 0, 0, 0.5)", new gamejs.Rect([0, 0], SIZE), 0)
+               mainSurface.blit(paused, [SIZE[0] / 2 - paused.getSize()[0] / 2, SIZE[1] / 2 - paused.getSize()[1] / 2]);
+            }
+            
+            return;
+         }
+         
+         pausedDrawn = false;
          
          if (!controls.paused) {
             backgroundPos += 10 * (msDuration / 1000);    
@@ -88,23 +103,22 @@ function main() {
             
             ai.draw(mainSurface);
          } else {
+            gamejs.draw.rect(mainSurface, "rgba(255, 0, 0, 0.5)", new gamejs.Rect([0, 0], SIZE), 0)
             mainSurface.blit(gameOver, [SIZE[0] / 2 - gameOver.getSize()[0] / 2, SIZE[1] / 2 - gameOver.getSize()[1] / 2]);
          }
          
          hud.health = ship.health;
          hud.level = ai.level;
          hud.draw(mainSurface);
-         
-         if (controls.paused) {
-            // Draw paused overlay
-            gamejs.draw.rect(mainSurface, "rgba(0, 0, 0, 0.5)", new gamejs.Rect([0, 0], SIZE), 0)
-            mainSurface.blit(paused, [SIZE[0] / 2 - paused.getSize()[0] / 2, SIZE[1] / 2 - paused.getSize()[1] / 2]);
-         }
    };
    gamejs.time.fpsCallback(tick, this, 45);
    
    var collisionTick = function (msDuration) {
       var collisions;
+      
+      if (controls.paused) {
+         return;
+      }
       
       collisions = gamejs.sprite.groupCollide(ship.weapons, ai.ships, true);
       collisions.forEach(function (collision) {

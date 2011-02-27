fc8bca51713525665bde1dc0752e501ed4deb8a9
Daniel G. Taylor <dan@programmer-art.org>
2011-02-27 13:40:50 -0500

    Cache HUD text until it changes so new text isn't rendered out each tick

--- screen.js
+++ screen.js
@@ -8,9 +8,20 @@ var Hud = function (size) {
    this.healthIcon = gamejs.image.load('images/health-hud.png');
    this.size = size;
    
+   var cachedScore = -1;
+   var scoreSurface = null;
+   var cachedLevel = -1;
+   var levelSurface = null;
+   
    this.draw = function (surface) {
-      var scoreSurface = this.font.render(this.score, "#fff");
-      var levelSurface = this.font.render("Level " + (this.level + 1), "#fff");
+      if (this.score != cachedScore) {
+         cachedScore = this.score;
+         scoreSurface = this.font.render(this.score, "#fff");
+      }
+      if (this.level != cachedLevel) {
+         cachedLevel = this.level;
+         levelSurface = this.font.render("Level " + (this.level + 1), "#fff");
+      }
       
       surface.blit(this.healthIcon, [8, 8]);
       gamejs.draw.rect(surface, "#0c0", new gamejs.Rect([26 + 16, 19, Math.max(0, this.health), 4]), 0);
